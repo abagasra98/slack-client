3048b60dba85e481278cefa1502642af8c3a32f1
Abdul <abdulbagasra@gmail.com>
2018-05-16 16:48:23 -0500

    Add Login component

--- src/routes/Login.js
+++ src/routes/Login.js
@@ -2,46 +2,68 @@ import React from 'react';
 import { extendObservable } from 'mobx';
 import { observer } from 'mobx-react';
 import { Container, Header, Input, Button } from 'semantic-ui-react';
+import { gql, graphql } from 'react-apollo';
 
-export default observer(
-  class Login extends React.Component {
-    constructor(props) {
-      super(props);
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
 
-      extendObservable(this, {
-        email: '',
-        password: '',
-      });
+    extendObservable(this, {
+      email: '',
+      password: '',
+    });
+  }
+
+  onSubmit = async () => {
+    const { email, password } = this;
+    const response = await this.props.mutate({
+      variables: { email, password },
+    });
+    const { ok, token, refreshToken } = response.data.login;
+    if (ok) {
+      localStorage.setItem('token', token);
+      localStorage.setItem('refreshToken', refreshToken);
     }
+  };
 
-    onSubmit = () => {
-      const { email, password } = this;
-      console.log(email, password);
-    };
+  onChange = e => {
+    const { name, value } = e.target;
+    this[name] = value;
+  };
 
-    onChange = e => {
-      const { name, value } = e.target;
-      this[name] = value;
-    };
+  render() {
+    const { email, password } = this;
 
-    render() {
-      const { email, password } = this;
+    return (
+      <Container text>
+        <Header as="h2">Login</Header>
+        <Input name="email" onChange={this.onChange} value={email} placeholder="Email" fluid />
+        <Input
+          name="password"
+          onChange={this.onChange}
+          value={password}
+          placeholder="Password"
+          type="password"
+          fluid
+        />
+        <Button onClick={this.onSubmit}>Submit</Button>
+      </Container>
+    );
+  }
+}
 
-      return (
-        <Container text>
-          <Header as="h2">Login</Header>
-          <Input name="email" onChange={this.onChange} value={email} placeholder="Email" fluid />
-          <Input
-            name="password"
-            onChange={this.onChange}
-            value={password}
-            placeholder="Password"
-            type="password"
-            fluid
-          />
-          <Button onClick={this.onSubmit}>Submit</Button>
-        </Container>
-      );
+const loginMutation = gql`
+  mutation($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      ok
+      token
+      refreshToken
+      errors {
+        path
+        message
+      }
     }
-  },
-);
+  }
+`;
+
+export default graphql(loginMutation)(observer(Login));
