1223273e61a26f61671a78c9f86f4f00ac09da07
Abdul <abdulbagasra@gmail.com>
2018-05-15 22:39:55 -0500

    Set up GraphQL + Apollo

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { ApolloClient, createNetworkInterface, ApolloProvider } from 'react-apollo';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import registerServiceWorker from './registerServiceWorker';
+import Routes from './routes';
+
+const networkInterface = createNetworkInterface({
+  uri: 'http://localhost:8080/graphql',
+});
+
+const client = new ApolloClient({
+  networkInterface,
+});
+
+const App = (
+  <ApolloProvider client={client}>
+    <Routes />
+  </ApolloProvider>
+);
+
+ReactDOM.render(App, document.getElementById('root'));
 registerServiceWorker();
