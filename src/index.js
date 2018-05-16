2e106571ea2b5c8947229f6a86381616e7574b15
Abdul <abdulbagasra@gmail.com>
2018-05-16 00:46:39 -0500

    Add Register component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'semantic-ui-css/semantic.min.css';
 import { ApolloClient, createNetworkInterface, ApolloProvider } from 'react-apollo';
 
 import registerServiceWorker from './registerServiceWorker';
